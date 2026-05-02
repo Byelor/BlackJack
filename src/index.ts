5334afca6b3311fa56dbf4e7b6880275a7f7b214
Byelor <zaqw1234bmbmbm@gmail.com>
2026-05-02 18:51:45 +0300

    sometestings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {server} from "./server/server.js";
 
 server.listen(process.env.PORT, ()=>{
-    console.log(`server starts listening on port ${process.env.PORT}`);
+    console.log(`server starts listening on http://127.0.0.1:${process.env.PORT}`);
 })
 
 
